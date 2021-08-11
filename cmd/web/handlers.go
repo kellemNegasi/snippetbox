ab2913d9563f2b5d3d421b6571214231277b9fa7
kellem negasi <kellemnegasi@kellems-MacBook-Pro.local>
2021-08-11 23:46:40 +0300

    refactored html templates and added static resources

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+
 	if r.URL.Path != "/" {
+
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from snippetbox\n"))
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal error ", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal error ", 500)
+		return
+	}
 }
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
