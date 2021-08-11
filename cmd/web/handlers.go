bcb71ec8bfb02ee80ded6ac7be25dc56d73f4727
kellem negasi <kellemnegasi@kellems-MacBook-Pro.local>
2021-08-11 22:39:35 +0300

    refactored and restructured project

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +14,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox\n"))
 }
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet\n"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// w.Write([]byte("Display a specific snippet\n"))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d \n\n", id)
 }
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -22,14 +30,4 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte("Create a specific snippet\n"))
-}
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-	log.Println("Starting server at port 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
-
 }
