8c10d44393f3c0990a45aeca07a20f1c54f2d0e6
kellem negasi <kellemnegasi@kellems-MacBook-Pro.local>
2021-08-12 16:09:19 +0300

    added command line configuration for port address

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// define a command line flag with a name "addr" and default value :4000
+	addr := flag.String("addr", ":4000", "server address port")
+	// parse the command line flag to get the value of addr
+	flag.Parse()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-	log.Println("Starting server at port 4000")
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	log.Printf("Starting server at port %s", *addr)
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
