593fc1dc964670308398316a8c80ef3598aa9a71
kellem negasi <kellemnegasi@kellems-MacBook-Pro.local>
2021-08-12 23:50:20 +0300

    refactored the routers to a separeted file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,23 +20,18 @@ func main() {
 	// add leveled logging capability i.e info and error
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	infoLog.Printf("Starting server at port %s", *addr)
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
+	infoLog.Printf("Starting server at port %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
