f04e941adaca16b0b61b907294e7c4617827d6fa
kellem negasi <kellemnegasi@kellems-MacBook-Pro.local>
2021-08-14 21:41:37 +0300

    implemented adding default data to the footer

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
@@ -15,11 +16,19 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	}
 	buf := new(bytes.Buffer)
 	// Execute the template set, passing in any dynamic data.
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 	}
 	buf.WriteTo(w)
+}
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+
+	td.CurrentYear = time.Now().Year()
+	return td
 }
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
